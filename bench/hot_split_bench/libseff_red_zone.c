061dd0a048502b383efec98c42ea7b6ba8d0d4e7
Teodoro Freund <teodoro.freund@huawei.com>
2023-10-26 14:34:01 +0100

    Added native and graphs for hot_split

--- bench/hot_split_bench/libseff_red_zone.c
+++ bench/hot_split_bench/libseff_red_zone.c
@@ -1,5 +1,6 @@
 #define REPS 10000000
 #define BOTTOM_ARR 64
 #define CORO_SIZE 4096
+#define COROUTINE
 
 #include "libseff_common.inc"
--- bench/hot_split_bench/native.c
+++ bench/hot_split_bench/native.c
@@ -1,6 +1,5 @@
 #define REPS 10000000
-#define BOTTOM_ARR 2048
+#define BOTTOM_ARR 4096 * 2
 #define CORO_SIZE 4096
-#define COROUTINE
 
 #include "libseff_common.inc"
